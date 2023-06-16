32679dbdab2b9463bd5e88d4d985d60f3cde9719
Sabaudin Tozaj <116427939+st097@users.noreply.github.com>
2023-06-16 23:13:27 +0200

    Add files via upload

---
+++ people-counter/index.welcome.js
@@ -0,0 +1,10 @@
+let welcomeEl = document.getElementById("welcome-el")
+
+let me = "John"
+let greeting = "Hello"
+
+welcomeEl.innerText = greeting + " " + me
+
+ welcomeEl.innerText += " " + "👋"
+
+    
